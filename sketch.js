9202115f5005bf290a2953cf179e2b12065d770d
whitehat-G6 <88480121+WHJR-G6@users.noreply.github.com>
2021-09-17 01:05:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 var player;
 var platform;
 var player_image, platform_image;
@@ -11,22 +10,23 @@ function preload(){
 function setup() {
   createCanvas(500, 500);
   player = createSprite(250,300,20,50);
+  player.addImage("player",player_image)
 
   platform = createSprite(250,350,100,10);
   platform.addImage("platform", platform_image)
   platform.scale=0.15
-
 }
 
 function draw() {
   background('#F2F5B6');
+  
+  if(keyDown("up"))
+  {
+    player.velocityY=-10
+  }
 
   drawSprites();
 }
-
-
-
-
 
 
 
